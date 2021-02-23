d64f18c03a9d9ed675c5b0834ce4a19a7c8808f8
jms90 <justom78@gmail.com>
2021-02-23 15:43:51 +0100

    create seguimiento resource/migration/model/controller

---
+++ app/Http/Controllers/API/SeguimientoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Seguimiento;
+use Illuminate\Http\Request;
+
+class SeguimientoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Seguimiento  $seguimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Seguimiento $seguimiento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Seguimiento  $seguimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Seguimiento $seguimiento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Seguimiento  $seguimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seguimiento $seguimiento)
+    {
+        //
+    }
+}
